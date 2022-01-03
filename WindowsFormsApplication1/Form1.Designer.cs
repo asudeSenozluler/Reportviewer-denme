c1fe56a6b3e873202674093177260901a3065a5d
Asude Şenözlüler <senozlulerasude@gmail.com>
2022-01-03 14:37:51 +0300

    first commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.DbEntinityUrunDataSet1 = new WindowsFormsApplication1.DbEntinityUrunDataSet1();
+            this.TBLURUNBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.TBLURUNTableAdapter = new WindowsFormsApplication1.DbEntinityUrunDataSet1TableAdapters.TBLURUNTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.DbEntinityUrunDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TBLURUNBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "DataSet1";
+            reportDataSource1.Value = this.TBLURUNBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "WindowsFormsApplication1.Report1.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.Size = new System.Drawing.Size(591, 443);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // DbEntinityUrunDataSet1
+            // 
+            this.DbEntinityUrunDataSet1.DataSetName = "DbEntinityUrunDataSet1";
+            this.DbEntinityUrunDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // TBLURUNBindingSource
+            // 
+            this.TBLURUNBindingSource.DataMember = "TBLURUN";
+            this.TBLURUNBindingSource.DataSource = this.DbEntinityUrunDataSet1;
+            // 
+            // TBLURUNTableAdapter
+            // 
+            this.TBLURUNTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 443);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DbEntinityUrunDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TBLURUNBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource TBLURUNBindingSource;
+        private DbEntinityUrunDataSet1 DbEntinityUrunDataSet1;
+        private DbEntinityUrunDataSet1TableAdapters.TBLURUNTableAdapter TBLURUNTableAdapter;
+    }
+}
+
